966fbbe9a435662ea76344ac884e280514a0d533
Alan Lo <lo.alan@gmail.com>
2021-03-17 16:17:32 +1100

    updated azure2 to use tags

--- azure2/main.tf
+++ azure2/main.tf
@@ -17,13 +17,25 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "alanlo-tf2-resource"
   location = "Australia Southeast"
+
+  tags = {
+    Owner   = "alanlo"
+    Purpose = "training"
+    Client  = "L&D"
+  }
 }
 
 resource "azurerm_public_ip" "example" {
-    name                         = "example-public"
-    location                     = azurerm_resource_group.example.location
-    resource_group_name          = azurerm_resource_group.example.name
-    allocation_method = "Dynamic"
+  name                         = "example-public"
+  location                     = azurerm_resource_group.example.location
+  resource_group_name          = azurerm_resource_group.example.name
+  allocation_method = "Dynamic"
+
+  tags = {
+    Owner   = "alanlo"
+    Purpose = "training"
+    Client  = "L&D"
+  }
 }
 
 resource "azurerm_virtual_network" "example" {
@@ -31,6 +43,12 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  
+  tags = {
+    Owner   = "alanlo"
+    Purpose = "training"
+    Client  = "L&D"
+  }
 }
 
 resource "azurerm_subnet" "example" {
@@ -38,6 +56,12 @@ resource "azurerm_subnet" "example" {
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
   address_prefixes     = ["10.0.2.0/24"]
+
+  #tags = {
+  #  Owner   = "alanlo"
+  #  Purpose = "training"
+  #  Client  = "L&D"
+  #}
 }
 
 resource "azurerm_network_interface" "example" {
@@ -50,6 +74,12 @@ resource "azurerm_network_interface" "example" {
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.example.id
+  }
+
+  tags = {
+    Owner   = "alanlo"
+    Purpose = "training"
+    Client  = "L&D"
   }
 }
 
@@ -82,6 +112,11 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   custom_data = base64encode(file("myapp.sh"))
 
+  tags = {
+    Owner   = "alanlo"
+    Purpose = "training"
+    Client  = "L&D"
+  }
 }
 
 
