943b892ae3e1d8e232d3f4c5324bfa6da64ba4f3
Alan Lo <lo.alan@gmail.com>
2021-08-03 10:26:36 +1000

    the correct way to do lists

--- syntax/list_0.12/main.tf
+++ syntax/list_0.12/main.tf
@@ -3,31 +3,31 @@ variable "my_list" {
   default = ["a", "b", "c"]
 }
 
-variable "my_single" {
-  type    = string
-  default = "d"
-}
-
 variable "my_none" {
   type    = list(string)
   default = []
 }
 
+variable "my_single" {
+  type    = string
+  default = "d"
+}
+
 locals {
-  my_list   = [var.my_list]
+  my_list   = var.my_list
+  my_none   = var.my_none
   my_single = [var.my_single]
-  my_none   = [var.my_none]
 }
 
 output "my_list" {
   value = local.my_list
 }
 
-output "my_single" {
-  value = local.my_single
-}
-
 output "my_none" {
   value = local.my_none
+}
+
+output "my_single" {
+  value = local.my_single
 }
 
